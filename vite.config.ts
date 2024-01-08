198dd05dae0a7e30f38108bd885fb29d0e69de3c
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-08 12:00:56 +0530

    chore: demo build

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
-    },
-    copyPublicDir: false,
-  }
+  plugins: [react(), 
+    // dts({ include: ['lib'] })
+  ],
+  // build: {
+  //   lib: {
+  //     entry: resolve(__dirname, 'lib/main.ts'),
+  //     formats: ['es']
+  //   },
+  //   copyPublicDir: false,
+  // }
 })
