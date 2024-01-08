738f8e21098550157292e0f7a945265a196ebde2
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-08 10:06:13 +0530

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "../lib/styles/_mixins.scss"
+
+import Editor from '../lib/components/Editor'
+
+function App() {
+
+  return (
+    <>
+      <Editor />
+    </>
+  )
+}
+
+export default App
